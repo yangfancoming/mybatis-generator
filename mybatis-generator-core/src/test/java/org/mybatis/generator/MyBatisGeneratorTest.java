668eff9c56caa44596eee38797bb5d2966568159
fan.yang <34465021+jwfl724168@users.noreply.github.com>
2019-12-01 17:53:50 +0800

    找到断点调试 入口测试类

--- mybatis-generator-core/src/test/java/org/mybatis/generator/MyBatisGeneratorTest.java
+++ mybatis-generator-core/src/test/java/org/mybatis/generator/MyBatisGeneratorTest.java
@@ -3,6 +3,10 @@ package org.mybatis.generator;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -15,17 +19,75 @@ import org.mybatis.generator.config.JDBCConnectionConfiguration;
 import org.mybatis.generator.config.ModelType;
 import org.mybatis.generator.config.xml.ConfigurationParser;
 import org.mybatis.generator.exception.InvalidConfigurationException;
+import org.mybatis.generator.exception.XMLParserException;
 import org.mybatis.generator.internal.DefaultShellCallback;
 
 public class MyBatisGeneratorTest {
+
+    // mybatis 自动生成测试类
+    @Test
+    public void tes111t() throws IOException, XMLParserException, InvalidConfigurationException, SQLException, InterruptedException {
+        List<String> warnings = new ArrayList<>();
+//        File configFile = new File("scripts/goatConfig.xml");
+        File configFile = new File(getClass().getClassLoader().getResource("scripts/goatConfig.xml").getFile());
+        ConfigurationParser cp = new ConfigurationParser(warnings);
+        Configuration config = cp.parseConfiguration(configFile);
+        DefaultShellCallback callback = new DefaultShellCallback(true);
+        MyBatisGenerator myBatisGenerator = new MyBatisGenerator(config, callback, warnings);
+        myBatisGenerator.generate(null);
+    }
+
+    // mybatis 自动生成测试类
+    @Test
+    public void mybatisGeneratorTest() {
+        List<String> warnings = new ArrayList<>();
+        String genCfg = "scripts/goatConfig.xml";
+        File configFile = new File(getClass().getClassLoader().getResource(genCfg).getFile());
+        ConfigurationParser cp = new ConfigurationParser(warnings);
+        Configuration config = null;
+        try {
+            config = cp.parseConfiguration(configFile);
+        } catch (IOException e) {
+            e.printStackTrace();
+        } catch (XMLParserException e) {
+            e.printStackTrace();
+        }
+        DefaultShellCallback callback = new DefaultShellCallback(true);
+        MyBatisGenerator myBatisGenerator = null;
+        try {
+            myBatisGenerator = new MyBatisGenerator(config, callback, warnings);
+        } catch (InvalidConfigurationException e) {
+            e.printStackTrace();
+        }
+        try {
+            myBatisGenerator.generate(null);
+        } catch (SQLException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+
+
+
+
+    @Test
+    public void test() throws Exception {
+        List<String> warnings = new ArrayList<>();
+        ConfigurationParser cp = new ConfigurationParser(warnings);
+        Configuration config = cp.parseConfiguration(this.getClass().getClassLoader().getResourceAsStream("scripts/goatConfig.xml"));
+        DefaultShellCallback shellCallback = new DefaultShellCallback(true);
+        MyBatisGenerator myBatisGenerator = new MyBatisGenerator(config, shellCallback, warnings);
+        myBatisGenerator.generate(null, null, null, false);
+    }
 
     @Test
     public void testGenerateMyBatis3WithInvalidConfig() throws Exception {
         List<String> warnings = new ArrayList<>();
         ConfigurationParser cp = new ConfigurationParser(warnings);
-//        Configuration config = cp.parseConfiguration(this.getClass().getClassLoader().getResourceAsStream("generatorConfigMyBatis3_badConfig.xml"));
-        Configuration config = cp.parseConfiguration(this.getClass().getClassLoader().getResourceAsStream("scripts/goatConfig.xml"));
-
+        Configuration config = cp.parseConfiguration(this.getClass().getClassLoader().getResourceAsStream("generatorConfigMyBatis3_badConfig.xml"));
         DefaultShellCallback shellCallback = new DefaultShellCallback(true);
 
         InvalidConfigurationException e = 
